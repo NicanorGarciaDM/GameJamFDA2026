02ed8985f32b3fbd8dabb192f5bcc1292dd91415
NicanorGarciaDM <nicanorgarcia299@gmail.com>
2026-08-20 16:06:22 -0300

    Update pantalla_1.gd

--- juegojam/pantalla_1.gd
+++ juegojam/pantalla_1.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_flechita_pressed() -> void:
-	get_tree().change_scene_to_file("res://pantallaEscritorio")
+	get_tree().change_scene_to_file("res://escritorio.tscn")
