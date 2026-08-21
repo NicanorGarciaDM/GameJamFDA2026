1f113b00a651b81149d45109d7f2ad86fe4c26a5
NicanorGarciaDM <nicanorgarcia299@gmail.com>
2026-08-21 03:41:20 -0300

    t

--- juegojam/scripts/pantalla_1.gd
+++ juegojam/scripts/pantalla_1.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_flechita_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/escritorio.tscn")
+func _on_texture_rect_pressed() -> void:
+	get_tree().change_scene_to_file("res://escenas/pantalla2.tscn")
